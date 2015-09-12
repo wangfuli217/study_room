285cba99ca2559dd64661499a0cf928e138c9a27
cloudrain21 <cloudrain21@gmail.com>
2015-09-13 07:58:52 +0900

    examples from expert c++

--- c++/expert_c++/Spreadsheet/Spreadsheet.cpp
+++ c++/expert_c++/Spreadsheet/Spreadsheet.cpp
@@ -9,6 +9,52 @@ Spreadsheet::Spreadsheet(int inWidth, int inHeight) : mWidth(inWidth), mHeight(i
     }
 }
 
+void Spreadsheet::copyFrom(const Spreadsheet& src)
+{
+    mWidth = src.mWidth;
+    mHeight = src.mHeight;
+
+    mCells = new SpreadsheetCell*[mWidth];
+    for(int i=0; i<mWidth; i++)
+    {
+	mCells[i] = new SpreadsheetCell[mHeight];
+    }
+
+    for(int i=0; i<mWidth; i++)
+    {
+	for(int j=0; j<mWidth; j++)
+	{
+	    mCells[i][j] = src.mCells[i][j];
+	}
+    }
+}
+
+// deep copy constructor
+Spreadsheet::Spreadsheet(const Spreadsheet& src)
+{
+    copyFrom(src);
+}
+
+Spreadsheet& Spreadsheet::operator=(const Spreadsheet& rhs)
+{
+    if(this == &rhs)
+    {
+	return *this;
+    }
+
+    for(int i=0; i<mWidth; ++i)
+    {
+	delete [] mCells[i];
+    }
+
+    delete [] mCells;
+    mCells = nullptr;
+
+    copyFrom(rhs);
+
+    return *this;
+}
+
 Spreadsheet::~Spreadsheet()
 {
     for(int i=0; i<mWidth; i++)
@@ -36,4 +82,9 @@ SpreadsheetCell Spreadsheet::getCellAt(int x, int y)
 	throw std::out_of_range("");
     }
     return mCells[x][y];
+}
+
+int main()
+{
+    Spreadsheet s1(3,5); 
 }
--- c++/expert_c++/Spreadsheet/Spreadsheet.h
+++ c++/expert_c++/Spreadsheet/Spreadsheet.h
@@ -1,11 +1,16 @@
 #include "SpreadsheetCell.h"
 
-class SpreadsheetCell{
+class Spreadsheet{
 public:
     Spreadsheet(int inWidth, int inHeight);
+    Spreadsheet(const Spreadsheet& src);
+    Spreadsheet& operator=(const Spreadsheet& rhs);
     ~Spreadsheet();
-    void setCellAt(int x, int y, const SpreadsheetCell& cell);
+    void setCellAt(int x, int y, const Spreadsheet& cell);
     SpreadsheetCell getCellAt(int x, int y);
+private:
+    void setCellAt(int x, int y, const SpreadsheetCell& cell);
+    void copyFrom(const Spreadsheet& src);
 protected:
     bool inRange(int val, int upper);
     int mWidth, mHeight;
