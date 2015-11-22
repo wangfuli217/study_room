1268a0aede41942bf84ff4771eb3d7c8c0228c6c
cloudrain21 <cloudrain21@gmail.com>
2015-11-23 06:30:51 +0900

    add sample

---
+++ libc++/main.cpp
@@ -0,0 +1,18 @@
+#include "String.h"
+#include <iostream>
+
+int main()
+{
+  cr::String s;
+ 
+  std::string s1 = s;
+  std::wstring s2 = s;
+  s = "hello";
+  s = L"hello";
+  s += ' ';
+  s += 'a';
+  s += L'a';
+ 
+  cr::String s3("world");
+  std::cout << s << " " << s3 << std::endl;
+}
