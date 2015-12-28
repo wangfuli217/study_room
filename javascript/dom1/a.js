dfbd12248d4ef9a3182b5a19b624d72c4cafe570
cloudrain21 <cloudrain21@gmail.com>
2015-12-29 06:19:03 +0900

    dom example

---
+++ javascript/dom1/a.js
@@ -0,0 +1,4 @@
+document.write("appCodeName : ", + navigator.appCodeName, "<br /");
+document.write("appName : ", + navigator.appName, "<br /");
+document.write("appVersion : ", + navigator.appVersion, "<br /");
+document.write("language: ", + navigator.language, "<br /");
