4f3c060112716cd1c2a29169d585fa193af18102
dplee <dplee@localhost.localdomain>
2016-01-29 16:02:59 +0900

    cluster db start

--- python/prepare_cluster/sundb_cluster_start.sh
+++ python/prepare_cluster/sundb_cluster_start.sh
@@ -1,34 +1,82 @@
-if [ $# -lt 1 ]; then
-    echo "ctest.sh drive1/drive2/node"
+### Group G1 : member g1n1, g1n2
+### Group G2 : member g2n1
+
+export PRODUCT_HOME=/home/dplee/work/sundb_trunk
+export SUNDB_HOME=$PRODUCT_HOME/Gliese/home
+export PATH=$SUNDB_HOME/bin:$PATH
+
+if [ $1 = 'init' ]; then
+    cleandb.sh
+    find $SUNDB_HOME -name "*.dbf" -exec rm {} \;
+    find $SUNDB_HOME -name "*.log" -exec rm {} \;
+    find $SUNDB_HOME -name "*.ctl" -exec rm {} \;
     exit
 fi
 
-### (1) cluster drive node ###
-if [ $1 = 'drive1' ]; then
-    # create db & cluster group
-    gcreatedb --cluster --db_name=SUNDB --home=$SUNDB_HOME/g1n1_home --member=g1n1 --host=127.0.0.1 --port=10101
-    gsql --as sysdba --dsn=CLUSTER << EOF
-        \cstartup local open;
-        alter system mount global database;
-        create cluster group g1 cluster member g1n1 host '127.0.0.1' port 10101;
-        \quit
-EOF
-### (2) cluster non-drive node ###
-elif [ $1 = 'node' ]; then
-    # create db and global mount
-    gcreatedb --cluster --db_name=SUNDB --home=$SUNDB_HOME/g1n2_home --member=g1n2 --host=127.0.0.1 --port=10102
+if [ $# -lt 1 ]; then
+    echo "ctest.sh node11/node12/node21/node22/drive"
+    exit
+fi
+
+mount_global()
+{
     gsql --as sysdba --dsn=CLUSTER << EOF
         \cstartup local open;
         alter system mount global database;
         \quit;
 EOF
+    glsnr --start
+}
+
+##################################
+### (1) cluster non-drive node ###
+##################################
+if [ $1 = 'node11' ]; then
+    export SUNDB_DATA=$PRODUCT_HOME/Gliese/home/g1n1_home
+
+    # create db and global mount
+    gcreatedb --cluster --db_name=SUNDB --home=$SUNDB_DATA --member=g1n1 --host=127.0.0.1 --port=10101
+    mount_global
+##################################
+### (2) cluster non-drive node ###
+##################################
+elif [ $1 = 'node12' ]; then
+    export SUNDB_DATA=$PRODUCT_HOME/Gliese/home/g1n2_home
+
+    # create db and global mount
+    gcreatedb --cluster --db_name=SUNDB --home=$SUNDB_DATA --member=g1n2 --host=127.0.0.1 --port=10102
+    mount_global
+##################################
+### (3) cluster non-drive node ###
+##################################
+elif [ $1 = 'node21' ]; then
+    export SUNDB_DATA=$PRODUCT_HOME/Gliese/home/g2n1_home
+
+    # create db and global mount
+    gcreatedb --cluster --db_name=SUNDB --home=$SUNDB_DATA --member=g2n1 --host=127.0.0.1 --port=10201
+    mount_global
+##################################
+### (4) cluster non-drive node ###
+##################################
+elif [ $1 = 'node22' ]; then
+    export SUNDB_DATA=$PRODUCT_HOME/Gliese/home/g2n2_home
+
+    # create db and global mount
+    gcreatedb --cluster --db_name=SUNDB --home=$SUNDB_DATA --member=g2n2 --host=127.0.0.1 --port=10202
+    mount_global
 fi
 
-### (3) cluster drive node ###
-if [ $1 = 'drive2' ]; then
+##################################
+### (5) cluster drive node     ###
+##################################
+if [ $1 = 'drive' ]; then
+    export SUNDB_DATA=$PRODUCT_HOME/Gliese/home/g1n1_home
+
     # add non-drive node to cluster group and open global database
     gsql --as sysdba --dsn=CLUSTER << EOF
-        alter cluster group g1 add cluster member g1n2 host '192.168.0.206' port 10102;
+        create cluster group g1 cluster member g1n1 host '127.0.0.1' port 10101;
+        alter cluster group g1 add cluster member g1n2 host '127.0.0.1' port 10102;
+        create cluster group g2 cluster member g2n1 host '192.168.0.206' port 10201;
         alter system open global database;
         \quit
 EOF
