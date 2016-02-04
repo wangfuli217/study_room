e4b1276110f2dcdf24cfdbe94d67310bf490be58
cloudrain21 <cloudrain21@gmail.com>
2016-02-04 15:17:32 +0900

    sundb cluster script

--- python/prepare_cluster/sundb_cluster_start.sh
+++ python/prepare_cluster/sundb_cluster_start.sh
@@ -1,13 +1,22 @@
 ### 1. ctest.sh clean
 ### 2. ctest.sh g1n1
 ### 3. ctest.sh g1n2
+### 3. ctest.sh g2n1
 ### 4. ...
-### 5. ctest.sh drive
+### 5. ctest.sh drive g1n1 g1n2 g2n1...
 
 export PRODUCT_HOME=/home/dplee/work/sundb_trunk
 export SUNDB_HOME=$PRODUCT_HOME/Gliese/home
 export PATH=$SUNDB_HOME/bin:$PATH
 
+if [ $# -lt 1 ]; then
+    echo "No argument"
+    exit
+fi
+
+##################################
+### (1) cleandb & kill procs   ###
+##################################
 if [ $1 = 'clean' ]; then
     cleandb.sh
     find $SUNDB_HOME -name "*.dbf" -exec rm {} \;
@@ -21,68 +30,160 @@ if [ $# -lt 1 ]; then
     exit
 fi
 
+##################################
+### (2) cluster non-drive node ###
+##################################
 mount_global()
 {
+    # create db and global mount
+    gcreatedb --cluster --db_name=SUNDB --home=$SUNDB_HOME/"$1"_home --member=$1 --host=127.0.0.1 --port=$2
     gsql --as sysdba --dsn=$1 << EOF
         \cstartup local open;
         alter system mount global database;
         \quit;
 EOF
-    glsnr --start
+    glsnr --start --home=$SUNDB_HOME/"$1"_home
 }
 
-##################################
-### (1) cluster non-drive node ###
-##################################
-if [ $1 = 'g1n1' ]; then
-    export SUNDB_DATA=$PRODUCT_HOME/Gliese/home/g1n1_home
+if [ `echo "$1" | cut -c 1-1` = 'g' ]; then
+    groupno=`echo $1 | cut -c 2-2`
+    memberno=`echo $1 | cut -c 4-4`
 
-    # create db and global mount
-    gcreatedb --cluster --db_name=SUNDB --home=$SUNDB_DATA --member=g1n1 --host=127.0.0.1 --port=10101
-    mount_global $1
-##################################
-### (2) cluster non-drive node ###
-##################################
-elif [ $1 = 'g1n2' ]; then
-    export SUNDB_DATA=$PRODUCT_HOME/Gliese/home/g1n2_home
+    port="10""$groupno""0""$memberno"
 
-    # create db and global mount
-    gcreatedb --cluster --db_name=SUNDB --home=$SUNDB_DATA --member=g1n2 --host=127.0.0.1 --port=10102
-    mount_global $1
-##################################
-### (3) cluster non-drive node ###
-##################################
-elif [ $1 = 'g2n1' ]; then
-    export SUNDB_DATA=$PRODUCT_HOME/Gliese/home/g2n1_home
-
-    # create db and global mount
-    gcreatedb --cluster --db_name=SUNDB --home=$SUNDB_DATA --member=g2n1 --host=127.0.0.1 --port=10201
-    mount_global $1
-##################################
-### (4) cluster non-drive node ###
-##################################
-elif [ $1 = 'g2n2' ]; then
-    export SUNDB_DATA=$PRODUCT_HOME/Gliese/home/g2n2_home
-
-    # create db and global mount
-    gcreatedb --cluster --db_name=SUNDB --home=$SUNDB_DATA --member=g2n2 --host=127.0.0.1 --port=10202
-    mount_global $1
+    mount_global $1 $port
 fi
 
 ##################################
-### (5) cluster drive node     ###
+### (3) cluster drive node     ###
 ##################################
-if [ $1 = 'drive' ]; then
-    export SUNDB_DATA=$PRODUCT_HOME/Gliese/home/g1n1_home
+groupname_list=""
+check_already_exist_groupname()
+{
+    for one in $groupname_list
+    do
+        if [ $1 = $one ]; then
+            return 1
+        fi
+    done
 
-    # add non-drive node to cluster group and open global database
-    gsql --as sysdba --dsn=CLUSTER << EOF
-        create cluster group g1 cluster member g1n1 host '127.0.0.1' port 10101;
-        alter cluster group g1 add cluster member g1n2 host '127.0.0.1' port 10102;
-        create cluster group g2 cluster member g2n1 host '192.168.0.206' port 10201;
+    return 0
+}
+
+get_groupname_list()
+{
+    # get group name list out of node list
+    argcount=$#
+    idx=2
+    while :
+    do
+        groupname=`echo ${!idx} | cut -c 1-2`
+        if [ $groupname"X" = "X" ]; then
+            break;
+        else
+            check_already_exist_groupname $groupname
+
+            if [ $? -eq 0 ]; then
+                groupname_list="$groupname_list"" $groupname"
+            fi
+        fi
+
+        idx=`expr $idx + 1`
+    done
+}
+
+member_list=""
+get_member_list()
+{
+    argcount=$#
+    idx=2
+
+    while :
+    do
+        if [ $idx -gt $argcount ]; then
+            break;
+        else
+            member_list="$member_list"" ${!idx}"
+        fi
+
+        idx=`expr $idx + 1`
+    done
+}
+
+check_groupname_list_valid()
+{
+    if [ "$groupname_list""X" = "X" ]; then
+        echo "No group list. You must give all the nodes you already created."
+        echo "Try again. exiting..."
+        exit
+    else
+        echo "Group List : $groupname_list"
+    fi
+}
+
+created_group=""
+
+ret=0
+check_group_created()
+{
+    for one in $created_group
+    do
+        if [ $one = $1 ]; then
+            ret=1
+            return
+        fi
+    done
+
+    ret=0
+}
+
+if [ $1 = 'drive' ]; then
+    #export SUNDB_DATA=$PRODUCT_HOME/Gliese/home/"$1""_home"
+
+    get_groupname_list $*
+    check_groupname_list_valid
+    get_member_list $*
+
+    drive_node=""
+    first=0
+
+    for group in $groupname_list
+    do
+        for member in $member_list
+        do
+            if [ $first -eq 0 ]; then
+                drive_node=$member
+                first=1
+            fi
+
+            if [ $group = `echo $member | cut -c 1-2` ]; then
+                groupno=`echo $member | cut -c 2-2`
+                memberno=`echo $member | cut -c 4-4`
+
+                port="10""$groupno""0""$memberno"
+
+                check_group_created $group
+
+                if [ $ret -eq 0 ]; then
+                    gsql --as sysdba --dsn=$drive_node << EOF
+                        create cluster group $group cluster member $member host '127.0.0.1' port $port;
+                        \quit
+EOF
+                    created_group=$created_group" $group"
+                else
+                    gsql --as sysdba --dsn=$drive_node << EOF
+                        alter cluster group $group add cluster member $member host '127.0.0.1' port $port;
+                        \quit
+EOF
+                fi
+            fi
+        done
+    done
+
+    gsql --as sysdba --dsn=$drive_node << EOF
         alter system open global database;
         \quit
 EOF
+
     cinit.sh
-    glsnr --start
 fi
