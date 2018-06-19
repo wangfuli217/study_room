2b3ac07bba4212fa175667ca0786516630180cc9
cloudrain21 <cloudrain21@gmail.com>
2018-06-20 08:20:07 +0900

    neutral network - python machine learning

---
+++ machine_learning/neural_network/recognize_character/nn.py
@@ -0,0 +1,20 @@
+import numpy
+import scipy.special   # for sigmoid function
+import matplotlib.pyplot
+%matplotlib inline
+
+class neuralNetwork:
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+        self.wih = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
+        self.who = numpy.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
+
+        self.lr = learningrate
+        self.activation_function = lambda x: scipy.special.expit(x)
+        pass
+
+    def train(self, inputs_list, targets_list):
+
+
