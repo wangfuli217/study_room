8f42e80ad6595b8d13fa77342d5e24d639b6434a
cloudrain21 <cloudrain21@gmail.com>
2018-06-30 14:17:54 +0900

    n 번째 palindrome 수 구하기

--- algorithm/palindrome_number.py
+++ algorithm/palindrome_number.py
@@ -23,45 +23,111 @@ n은 1부터 시작하며 크기에는 제한이 없다.
 '''
 
 import sys
+from functools import reduce
+
+'''
+# too slow
+def isPalin( number ):
+    numstr = str(number)
+    numlen = len(numstr)
+
+    for i in range(numlen):
+        if i >= ((numlen-1)-i):
+            return 1
+        if numstr[i] != numstr[(numlen-1)-i]:
+            return 0
 
 def findPalin( n ):
     number = 0
     nth = 0
 
     while 1:
-        numstr = str(number)
-        numlen = len(numstr)
-
-        for i in range(numlen):
-            if i >= ((numlen-1)-i):
-                #print("palin {}".format(number))
-                nth += 1
-                break
-            if numstr[i] != numstr[(numlen-1)-i]:
-                break
-
+        if isPalin(number) == 1:
+            #print("palin {}".format(number))
+            nth += 1
         if nth == n:
             return number
-
         number += 1
+'''
+
+# input  : 숫자의 자릿수 k
+# output : k 자릿수의 수 중에서 palindrome 수의 갯수
+def getPalinCount( k ):
+    if k < 1:
+        return 0
+    else:
+        return ( 9 * 10 ** int((k - 1)/2) )
+
+# 1 ~ k 자릿수까지의 palindrome 수의 총 갯수 합
+def getPalinSum( k ):
+    return ( sum( [ getPalinCount(i+1) for i in range(k) ] ) + 1 )  # plus 1 for 0
 
 if __name__ == "__main__":
-    n = int(sys.argv[1])
+    n = int(sys.argv[1]) # n > 0
 
-    ret = findPalin(n)
-    print(ret)
+    k = 0
+    sum_palin = 0
+
+    # n 번째 palindrome 수가 몇자리 수인지 찾는다.
+    while sum_palin < n:
+        k += 1
+        sum_palin = getPalinSum(k)
+
+    print("==> {} 번째 palin 자릿수: {}".format(n,k), end='')
+
+    if k == 1:
+        print(", result : {}".format(n-1))
+        sys.exit(0)
+
+    # k 자릿수의 수 중에서 n 번째 palindrome 수를 찾기 위해
+    # 앞쪽의 수부터 결정하면서 범위를 좁혀나간다.
+    base_sum = getPalinSum(k-1)
+    remain_n = n - base_sum
+
+    val_str = ""
+    val_str_r = ""
+    s = 1  # 맨 앞자리는 0 이 올수 없다.
+    sum = 0
+
+    while k > 0:
+        interval = int(getPalinCount(k) / 9)
+        for i in range(s, 10):
+            sum += interval
+
+            if sum >= remain_n:
+                val_str += str(i)
+                if k != 1:
+                    val_str_r += str(i)
+                k = k - 2
+                s = 0
+                sum = 0
+                remain_n -= (sum - interval)
+                break
+
+    print(", result : {}".format(val_str + val_str_r[::-1]))
 
 '''
- 위의 로직은 성능이 너무 안나온다.
+ 위의 too slow 로직은 성능이 너무 안나온다.
  아래의 규칙을 이용하면 되지 않을까?
 
- 한 자리   -> 10
- 두 자리   -> 9
- 세 자리   -> 9 x 10
- 4 자리    -> 9 x 10
- 5 자리    -> 9 x 10 x 10
- 6 자리    -> 9 x 10 x 10
- 7 자리    -> 9 x 10 x 10 x 10
- 8 자리    -> 9 x 10 x 10 x 10
+ 0        -> 1 개
+ 1 자리수 -> ( 9 x 10 ** 0 ) 개 (palindrome 수 갯수) (1~9)
+ 2 자리수 -> ( 9 x 10 ** 0 ) 개
+ 3 자리수 -> ( 9 x 10 ** 1 ) 개
+ 4 자리수 -> ( 9 x 10 ** 1 ) 개
+ 5 자리수 -> ( 9 x 10 ** 2 ) 개
+ 6 자리수 -> ( 9 x 10 ** 2 ) 개
+ 7 자리수 -> ( 9 x 10 ** 3 ) 개
+ 8 자리수 -> ( 9 x 10 ** 3 ) 개
  ...
+
+ 10 의 승수를 곱하는 함수
+ f(1) = 0
+ f(2) = 0
+ f(3) = 1
+ f(4) = 1
+ f(5) = 2
+ f(6) = 2
+ ...
+ f(x) = int( (x-1) / 2 )
 '''
