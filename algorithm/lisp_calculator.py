a4ab808487bfa9b159dcca65615f4f1e319669f3
cloudrain21 <cloudrain21@gmail.com>
2018-07-11 07:48:39 +0900

    LISP 계산기

--- algorithm/lisp_calculator.py
+++ algorithm/lisp_calculator.py
@@ -11,53 +11,77 @@ def calc_list(l, operator):
     elif operator == '*':
         v = l[0]
         for i in range(1,len(l)):
-            v += l[i]
+            v *= l[i]
         return v
     elif operator == '/':
         v = l[0]
         for i in range(1,len(l)):
             v /= l[i]
-        return v
+        return int(v)
     else:
         return sum(l)
 
-def process_str(s):
+def process_str(s,depth):
+    s = s.strip()
+    #print("{}{}".format('  '*depth, s))
+    depth += 1
+
+    if s[0] == '(' and s[len(s) - 1] != ')':
+        s = s[1:]
+    if s[0] != '(' and s[len(s) - 1] == ')':
+        s = s[:-1]
+    if s[0] == '(' and s[len(s) - 1] == ')':
+        s = s[1:-1]
+
+    l = []
     operator = ''
     digit = ''
-    is_open = 0
+    digit_count = 0
+    open_count = 0
     ret = 0
-    skip_count = 0
-    count = 0
+    x = -1
 
     for i in range(len(s)):
-        if skip_count > 0:
-            skip_count -= 1
+        if i <= x:
             continue
-        count += 1
         if s[i] == '(':
-            if is_open == 0:
-                l = []
-                is_open = 1
-            else:
-                (digit,skip_count) = process_str(s[i:])
-	        l.append(int(digit))
-                is_open = 0
+            x = i
+            open_count += 1
+            while open_count != 0:
+                x += 1
+                if s[x] == ')': open_count -= 1
+                if s[x] == '(': open_count += 1
+            digit = process_str(s[i:x+1], depth)
+            l.append(int(digit))
+            digit_count += 1
+            digit = ''
         elif s[i] in ['+', '-', '*', '/']:
             operator = s[i]
         elif s[i].isdigit():
             digit += s[i]
         elif s[i] == ' ':
             if digit != '':
-	        l.append(int(digit))
+                #print("{}{}".format('  '*depth, digit))
+                l.append(int(digit))
+                digit_count += 1
                 digit = ''
-        elif s[i] == ')':
-            ret = calc_list(l,operator)
-            return (ret,count-1)
         else:
             continue
 
-raw_s = raw_input()
+    if digit != '':
+        #print("{}{}".format('  '*depth, digit))
+        l.append(int(digit))
+        digit_count += 1
+        digit = ''
 
-v = process_str(raw_s)
+    if digit_count == 0:
+        return 0
+    else:
+        return calc_list(l,operator)
 
-print(v)
+raw_s = input('> ')
+
+depth = 0
+v = process_str(raw_s,depth)
+
+print("result : {}".format(v))
