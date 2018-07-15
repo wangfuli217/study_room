8291d8d715ff5e9fca789954595991d3c837ec12
cloudrain21 <cloudrain21@gmail.com>
2018-07-16 08:34:05 +0900

    짝수 피보나치 수열 합 구하기

--- algorithm/even_fibonacci_numbers.py
+++ algorithm/even_fibonacci_numbers.py
@@ -18,10 +18,23 @@ sumv = 0
 n = 1
 
 while 1:
-    f = fibo(n)
+    f = fibo(n)  # 매번 이렇게 구할 필요가 없지. (성능)
     if f > 4000000: break
     n += 1
     if f % 2 == 0:
         sumv += f
 
 print(sumv)
+
+
+'''
+좀더 간단하고 성능도 월등히 좋은 코드
+
+a, b = [0,1]
+sum = 0
+while b <= 4000000:
+    a = a + b
+    b, a = a, b
+    if b % 2 == 0: sum += b
+print(sum)
+'''
