400364f5796c607aa3b838c23989a31378c04a1b
cloudrain21 <cloudrain21@gmail.com>
2018-08-08 22:25:16 +0900

    사다리 게임

---
+++ algorithm/ladder_game.py
@@ -0,0 +1,57 @@
+'''
+사다리 게임
+
+input:
+a b c d e f
+| | | | | |
+|-| | |-| |
+| | |-| | |
+| |-| | |-|
+| | |-| | |
+|-| | |-| |
+| | | | | |
+1 2 3 4 5 6
+
+output:
+a - 5
+b - 2
+c - 3
+d - 6
+e - 1
+f - 4
+'''
+
+import numpy as np
+
+l = []
+while 1:
+    s = input()
+    if s == '': break
+    l.append(s)
+
+up = l[0].replace(' ', '')
+down = l[-1].replace(' ', '')
+
+l = l[1:-1]
+
+m = np.zeros( (len(l), len(up)), dtype=int )
+for i in range(len(l)):
+    for j in range(len(up)):
+        m[i,j] = -1
+
+row = 0
+for s in l:
+    for i in range(len(s)):
+        if s[i] == '-':
+            m[row,int(i/2)] = int(i/2) + 1
+            m[row,int((i+1)/2)] = int(i/2)
+    row += 1
+
+for c in range(len(up)):
+    col = c
+    row = 0
+    while row < len(l):
+        if m[row,col] != -1:
+            col = m[row,col]
+        row += 1
+    print("input ({}) -> output ({})".format(up[c], down[col]))
