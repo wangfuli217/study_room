0358ea44efb7e1245e150169ddf14c131051c592
cloudrain21 <cloudrain21@gmail.com>
2018-08-23 08:31:37 +0900

    사다리 게임

--- algorithm/ladder_game.py
+++ algorithm/ladder_game.py
@@ -55,3 +55,27 @@ for c in range(len(up)):
             col = m[row,col]
         row += 1
     print("input ({}) -> output ({})".format(up[c], down[col]))
+
+'''
+inp = '''a b c d e f
+| | | | | |
+|-| | |-| |
+| | |-| | |
+| |-| | |-|
+| | |-| | |
+|-| | |-| |
+| | | | | |
+1 2 3 4 5 6
+'''
+
+a, *b, c = inp.splitlines()
+c = list(c)
+for line in b:
+  for i, x in enumerate(line):
+    if i % 2 == 1 and x == '-':
+      c[i-1], c[i+1] = c[i+1], c[i-1]
+
+result = list(zip(a, c))[::2]
+for x, y in result:
+  print('{} - {}'.format(x,y))
+'''
